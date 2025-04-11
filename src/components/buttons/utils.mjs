ef2d791d0b3606f49592ceedd98c29ae77bdc236
MHDev <mhdev.fr@gmail.com>
2025-04-11 14:57:40 +0200

    variant more scalable and modulable user can assign any prop and if not the code will assign auto value to prop

--- src/components/buttons/utils.mjs
+++ src/components/buttons/utils.mjs
@@ -1,28 +1,30 @@
 export const variants = {
-    default : ({color : color, background : background}) => (
+    default : ({...props}) => (
         {
-            border : 'none',
-            borderRadius : '8px',
-            padding : '12px 15px',
-            cursor:'pointer',
-            color : color,
-            background : background,
-            display : 'flex',
-            flexDirection : 'column'
+            border : props.border ?? 'none',
+            cursor : props.cursor ?? 'pointer',
+            color : props.color ?? '#F4F4F4',
+            background : props.background ?? '#0C0C0C',
+            padding : props.padding ?? '.5rem 1rem',
+            borderRadius : props.borderRadius ?? 'calc(-2px + .5rem)',
+            margin : props.margin ?? '0',
+            display : props.display ?? 'flex',
+            flexDirection : props.flexDirection ?? 'center',
+            alignItems : props.alignItems ?? 'center',
+            gap : props.gap ?? '.5rem',
+            fontSize : props.fontSize ?? '.875rem',
+            fontWeight : props.fontWeight ?? '500',
+            lineHeight : props.lineHeight ?? '1.25rem'
         }
     ),
-    outline : ({color : color, outline : outline}) => (
+    outline : ({...props}) => (
         {
-            border : 'none',
-            borderRadius : '8px',
-            padding : '12px 15px',
-            cursor:'pointer',
-            color : color,
-            outline : outline,
-            background : 'none'
+
         }
     ),
     //to do
     // circle : {},
     // rounded : {},
+    // link : {},
+    // disable : {},
 }
